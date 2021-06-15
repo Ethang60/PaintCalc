98b152885be71aeca3a75163cb07d7eaae0385a2
Ethang60 <85894147+Ethang60@users.noreply.github.com>
2021-06-15 11:01:31 +0100

    Paint calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,14 +37,28 @@ public class Main {
             double height = (double) input4.nextDouble();
 
             double area = width * height;
-            System.out.println("Area of Wall is " + area + "Meters^2");
+            System.out.println("Area of Wall is " + area + " Meters^2");
 
             totalArea += area;
         }
-        System.out.println("Total Area of the Walls is" + totalArea);
+        System.out.println("Total Area of the Walls is " + totalArea);
 
         int noOfTins = (int) ((Math.round(totalArea / areaPerTin)) + Math.ceil((totalArea % areaPerTin) / areaPerTin));
         System.out.println("The number of tin(s) required is " + noOfTins);
+
+
+        switch (tinSize) {
+            case 1:
+                System.out.println("Price = £" + (3.5 * noOfTins));
+                break;
+            case 2:
+                System.out.println("Price = £" + (6 * noOfTins));
+                break;
+            case 3:
+                System.out.println("Price = £" + (10 * noOfTins));
+                break;
+        }
+
     }
 }
 
