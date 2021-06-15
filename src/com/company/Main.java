b13349bbf49091d23200e9595caf36e08dd9db48
Ethang60 <85894147+Ethang60@users.noreply.github.com>
2021-06-15 10:48:34 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,13 +9,9 @@ public class Main {
         System.out.println("Enter Number of Walls");
         int numWalls = (int) input1.nextDouble();
 
-        /*Scanner input2 = new Scanner(System.in);
-        System.out.println("Enter Number of Walls");
-        int tinSize = (int) input2.nextDouble();*/
-
-        Scanner input3 = new Scanner(System.in);
-        System.out.println("What Size Tin of Paint(1 = 2.5, 2 = 5, 3 = 10)?");
-        int tinSize = (int) input3.nextDouble();
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("What Size Tin of Paint? (1 = 2.5l, 2 = 5l, 3 = 10l)");
+        int tinSize = (int) input2.nextDouble();
 
 
         int areaPerTin = 0;
@@ -32,13 +28,13 @@ public class Main {
 
         double totalArea = 0;
         for (int i = 0; i < numWalls; i++) {
-            Scanner input4 = new Scanner(System.in);
+            Scanner input3 = new Scanner(System.in);
             System.out.println("Enter Width of Wall " + i + 1 + " (Meters)");
-            double width = (double) input4.nextDouble();
+            double width = (double) input3.nextDouble();
 
-            Scanner input5 = new Scanner(System.in);
+            Scanner input4 = new Scanner(System.in);
             System.out.println("Enter Height of wall " + i + 1 + " (Meters)");
-            double height = (double) input5.nextDouble();
+            double height = (double) input4.nextDouble();
 
             double area = width * height;
             System.out.println("Area of Wall is " + area + "Meters^2");
