cbe4975cb9fe487fe1c95be669b502fb6f8571e6
Ethang60 <85894147+Ethang60@users.noreply.github.com>
2021-06-16 11:48:08 +0100

    Paint calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+
+    public static String yesNO(int wallNum){
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Does Wall " + (wallNum + 1) + " Have a Void?(Yes or No)");
+        String answer = input2.nextLine();
+        answer = answer.toUpperCase();
+        return answer;
+    }
+
+    public static double areaCalc(int wallNum, double voidArea, int numWalls){
+        Scanner input3 = new Scanner(System.in);
+        double[] walls = new double[numWalls];
+        System.out.println("Enter Width of Wall " + (wallNum + 1) + " (m)");
+        double width = input3.nextDouble();
+        System.out.println("Enter Height of Wall " + (wallNum + 1) + " (m)");
+        double height = input3.nextDouble();
+        double area = (width * height) - voidArea;
+        walls[wallNum] = area;
+        System.out.println("Area of Wall is " + (walls[wallNum]) + " m^2");
+        return area;
+    }
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -27,34 +49,19 @@ public class Main {
                 areaPerTin = 20;
         }
 
-        double[] walls = new double[numWalls];
         double totalArea = 0;
         for (int i = 0; i < numWalls; i++) {
 
-            System.out.println("Does Wall " + (i + 1) + " Have a Void?(Yes or No)");
-            String answer = input.nextLine();
-            answer = answer.toUpperCase();
-
             double voidArea = 0;
-            if (answer.equals("YES")) {
+            if (yesNO(i).equals("YES")) {
                 System.out.println("Enter Width of Void (m)");
-                double widthWindow = (double) input.nextDouble();
+                double widthWindow = input.nextDouble();
                 System.out.println("Enter Height of Void (m)");
-                double heightWindow = (double) input.nextDouble();
+                double heightWindow = input.nextDouble();
                 voidArea = heightWindow * widthWindow;
             }
 
-            System.out.println("Enter Width of Wall " + (i + 1) + " (m)");
-            double width = input.nextDouble();
-
-
-            System.out.println("Enter Height of Wall " + (i + 1) + " (m)");
-            double height = input.nextDouble();
-
-            double area = (width * height) - voidArea;
-            walls[i] = area;
-            System.out.println("Area of Wall is " + (walls[i]) + " m^2");
-            totalArea += area;
+            totalArea += areaCalc(i, voidArea, numWalls);
         }
 
         System.out.println("Total Area of the Walls is " + totalArea + "m^2");
@@ -64,7 +71,7 @@ public class Main {
 
         switch (tinSize) {
             case 1:
-                System.out.println("Price = £" + (3.5 * noOfTins));
+                System.out.println("Price = £" + (3.50 * noOfTins));
                 break;
             case 2:
                 System.out.println("Price = £" + (6 * noOfTins));
@@ -72,7 +79,6 @@ public class Main {
             case 3:
                 System.out.println("Price = £" + (10 * noOfTins));
                 break;
-
         }
     }
 }
